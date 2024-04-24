1a4eafd1eb65e07729ae8869166650a2a577a243
earl86 <wang_chao86@126.com>
2024-04-24 09:34:25 +0800

    modify cms to cus

--- model/query_piece.go
+++ model/query_piece.go
@@ -1,9 +1,10 @@
 package model
 
 import (
+	"time"
+
 	jsoniter "github.com/json-iterator/go"
 	"github.com/pingcap/tidb/util/hack"
-	"time"
 )
 
 var jsonIterator = jsoniter.ConfigCompatibleWithStandardLibrary
@@ -30,6 +31,10 @@ const (
 	millSecondUnit = int64(time.Millisecond)
 )
 
+const (
+	microSecondUnit = int64(time.Microsecond)
+)
+
 var (
 	mqpp = NewMysqlQueryPiecePool()
 )
@@ -49,7 +54,7 @@ func NewBaseQueryPiece(
 	return
 }
 
-func (bqp *BaseQueryPiece) NeedSyncSend() (bool) {
+func (bqp *BaseQueryPiece) NeedSyncSend() bool {
 	return bqp.SyncSend
 }
 
@@ -57,7 +62,7 @@ func (bqp *BaseQueryPiece) SetNeedSyncSend(syncSend bool) {
 	bqp.SyncSend = syncSend
 }
 
-func (bqp *BaseQueryPiece) String() (*string) {
+func (bqp *BaseQueryPiece) String() *string {
 	content := bqp.Bytes()
 	contentStr := hack.String(content)
 	return &contentStr
@@ -73,7 +78,7 @@ func (bqp *BaseQueryPiece) Bytes() (content []byte) {
 	return bqp.jsonContent
 }
 
-func (bqp *BaseQueryPiece) GetSQL() (*string) {
+func (bqp *BaseQueryPiece) GetSQL() *string {
 	return nil
 }
 
