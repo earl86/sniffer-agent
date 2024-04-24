1d64a47cf3e3daa90ae60f81f819f2e2fb5b2669
earl86 <wang_chao86@126.com>
2024-04-24 18:37:30 +0800

    add sql Fingerprint

--- session-dealer/mysql/sqlpaser.go
+++ session-dealer/mysql/sqlpaser.go
@@ -2,11 +2,13 @@ package mysql
 
 import (
 	"bytes"
+	"fmt"
 
 	"github.com/pingcap/tidb/parser"
 	"github.com/pingcap/tidb/parser/ast"
 	"github.com/pingcap/tidb/parser/format"
 	driver "github.com/pingcap/tidb/types/parser_driver"
+	"github.com/pingcap/tidb/util/hack"
 )
 
 type FingerprintVisitor struct{}
@@ -22,12 +24,12 @@ func (f *FingerprintVisitor) Leave(n ast.Node) (node ast.Node, ok bool) {
 	return n, true
 }
 
-func processSQL(sql []byte) string {
+func processSQL(sql []byte) *string {
 	p := parser.New()
 	stmt, err := p.ParseOneStmt(sql, "", "")
 	if err != nil {
-		//fmt.Println("解析错误:" + err.Error())
-		return "error"
+		fmt.Println("解析错误:" + err.Error())
+		return err.Error()
 	}
 	stmt.Accept(&FingerprintVisitor{})
 
@@ -35,9 +37,9 @@ func processSQL(sql []byte) string {
 	restoreCtx := format.NewRestoreCtx(format.RestoreKeyWordUppercase|format.RestoreNameBackQuotes, buf)
 	err = stmt.Restore(restoreCtx)
 	if nil != err {
-		//fmt.Println("解析错误:" + err.Error())
-		return "error"
+		fmt.Println("解析错误:" + err.Error())
+		return err.Error()
 	}
 	//fmt.Println(buf.String())
-	return buf.String()
+	return &hack.String(buf.Bytes())
 }
