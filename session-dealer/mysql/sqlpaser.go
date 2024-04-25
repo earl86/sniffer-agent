7dfef4366a2ef90aea6c506913cea64542a9ad7d
earl86 <wang_chao86@126.com>
2024-04-25 12:43:23 +0800

    add sql fingerprint

--- session-dealer/mysql/sqlpaser.go
+++ session-dealer/mysql/sqlpaser.go
@@ -2,7 +2,6 @@ package mysql
 
 import (
 	"bytes"
-	"fmt"
 
 	"sniffer-agent/model"
 	"sniffer-agent/tidb/util/hack"
@@ -30,7 +29,7 @@ func processSQL(mqp *model.PooledMysqlQueryPiece, sql []byte) *model.PooledMysql
 	p := parser.New()
 	stmt, err := p.ParseOneStmt(string(sql), "", "")
 	if err != nil {
-		//fmt.Println("解析错误:" + err.Error())
+		// fmt.Println("解析错误:" + err.Error())
 		return mqp
 	}
 	stmt.Accept(&FingerprintVisitor{})
@@ -39,10 +38,10 @@ func processSQL(mqp *model.PooledMysqlQueryPiece, sql []byte) *model.PooledMysql
 	restoreCtx := format.NewRestoreCtx(format.RestoreKeyWordUppercase|format.RestoreNameBackQuotes, buf)
 	err = stmt.Restore(restoreCtx)
 	if nil != err {
-		//fmt.Println("解析错误:" + err.Error())
+		// fmt.Println("解析错误:" + err.Error())
 		return mqp
 	}
-	fmt.Println(buf.String())
+	// fmt.Println(buf.String())
 	querySQL := hack.String(buf.Bytes())
 	mqp.QuerySQLFinger = &querySQL
 	return mqp
